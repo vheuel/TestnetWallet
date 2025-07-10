645c63ddeeddb701e7d0fe273f7c495198a8501a
Vheüel <vheuels@gmail.com>
2025-07-10 23:30:59 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -25,8 +25,8 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
-    emptyOutDir: true,
+  outDir: "dist",
+  emptyOutDir: true,
   },
   server: {
     fs: {
